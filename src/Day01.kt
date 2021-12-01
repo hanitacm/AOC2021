68c8cb7a99a8314a0b65307115fdbd55b45943a9
anacordoba <ana-cordoba@cookpad.com>
2021-12-01 23:15:01 +0000

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,36 @@
 fun main() {
+    fun mapToListOfInteger(input: List<String>): MutableList<Int> {
+        val list = mutableListOf<Int>()
+        input.mapTo(list) { it.toInt() }
+        return list
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var previousDepth = 0
+        var counter = 0
+
+        mapToListOfInteger(input).map {
+            if (it > previousDepth) counter++
+            previousDepth = it
+        }
+
+        return counter - 1
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val list = mapToListOfInteger(input)
+        val resultList = mutableListOf<String>()
+
+        for (i in 0 until list.size - 2) {
+            resultList.add(list.slice(i..i + 2).sum().toString())
+        }
+
+        return part1(resultList)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
